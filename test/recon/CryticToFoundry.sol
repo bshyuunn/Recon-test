7198ae2bbdd7e599e24d9cd9b51052d4420b3920
bshyuunn <shyunb21@gmail.com>
2025-01-02 19:43:36 +0900

    add properties, foundry debugging script

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -14,4 +14,20 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     function testDemo() public {
         // TODO: Given any target function and foundry assert, test your results
     }
+
+    // https://getrecon.xyz/tools/medusa
+
+    // forge test --match-test test_invariant_receiver_balance_not_less_initial_0 -vv 
+    function test_invariant_receiver_balance_not_less_initial_0() public {
+    
+        vm.roll(30241);
+        vm.warp(360522);
+        vm.prank(0x0000000000000000000000000000000000030000);
+        naiveReceiverLenderPool_flashLoan(0x6804A3FF6bcf551fACf1a66369a5f8802B3C9C58, 0);
+        
+        vm.roll(36696);
+        vm.warp(368401);
+        vm.prank(0x0000000000000000000000000000000000010000);
+        invariant_receiver_balance_not_less_initial();
+    }
 }
--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -6,5 +6,11 @@ import {Asserts} from "@chimera/Asserts.sol";
 import {BeforeAfter} from "./BeforeAfter.sol";
 
 abstract contract Properties is BeforeAfter, Asserts {
-    
+    function invariant_receiver_balance_not_zero() public {
+        t(address(flashLoanReceiver).balance != 0, "invariant_receiver_balance_not_zero");
+    }
+ 
+    function invariant_receiver_balance_not_less_initial() public {
+       t(address(flashLoanReceiver).balance >= INIT_ETH_RECEIVER, "invariant_receiver_balance_not_less_initial");
+    }
 }
